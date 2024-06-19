2e3191d84df2d7ca0a24b0e532a7fd4c927de59d
Muneeb ur Rehman <136859466+Muneeb502@users.noreply.github.com>
2024-06-19 19:12:13 +0500

    add new code

--- UI_base_online_platform.py
+++ UI_base_online_platform.py
@@ -1,2 +1,12 @@
-def feedback():
-    input("inter feedback here")
+from flask import Flask ,request, render_template
+#from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5001)
