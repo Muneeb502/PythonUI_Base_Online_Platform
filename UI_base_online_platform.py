fc798883cc332e6317ca01f7f58ab61291e678b9
Muneeb ur Rehman <136859466+Muneeb502@users.noreply.github.com>
2024-06-20 17:25:52 +0500

    added new files

--- UI_base_online_platform.py
+++ UI_base_online_platform.py
@@ -1,11 +1,28 @@
 from flask import Flask ,request, render_template
-#from flask_sqlalchemy import SQLAlchemy
+import sqlite3 as sql
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/teacherlogin")
+def teacherlogin():
+    return render_template("teacherlogin.html")
+
+@app.route("/studentlogin")
+def studentlogin():
+    return render_template("studentlogin.html")
+
+@app.route("/teachersignup")
+def teachersignup():
+    return render_template("teachersign.html")
+
+@app.route("/studentsignup")
+def studentsignup():
+    return render_template("studentsign.html")
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,5 @@
+import sqlite3
+con = sqlite3.connect("mydata_base")
+
+print("creatdata base susscfully")
+
