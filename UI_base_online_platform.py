fab3aa0b02b3774ec26cc1cfec3fd16a498752c3
Muneeb ur Rehman <136859466+Muneeb502@users.noreply.github.com>
2024-06-25 20:05:46 +0500

    add two file and creat database

--- UI_base_online_platform.py
+++ UI_base_online_platform.py
@@ -1,6 +1,32 @@
 from flask import Flask ,request, render_template
-import sqlite3 as sql
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+#  configuration key
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:root@localhost:5432/projectno3_database"
+
+db = SQLAlchemy(app)
+
+# Define the Student model
+class Student(db.Model):
+   __tablename__ = "STUDENTS"
+   id = db.Column(db.Integer,primary_key = True,autoincremant=True)
+   name = db.Column(db.String(200),nullable= False)
+   email = db.Column(db.String(200), unique = True,nullable= False)
+   password = db.Column(db.String(200), nullable= False)
+
+# Define the Teacher model
+class Teacher(db.Model):
+   __tablename__ = "TEACHERS"
+   id = db.Column(db.Integer,primary_key = True,autoincremant=True)
+   name = db.Column(db.String(200),nullable= False)
+   email = db.Column(db.String(200), unique = True,nullable= False)
+   password = db.Column(db.String(200), nullable= False)
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
 
 app = Flask(__name__)
 
@@ -18,11 +44,16 @@ def studentlogin():
 
 @app.route("/teachersignup")
 def teachersignup():
+    
     return render_template("teachersign.html")
 
 @app.route("/studentsignup")
 def studentsignup():
     return render_template("studentsign.html")
+
+@app.route("/techer/dashboard")
+def tecacher_dashboard():
+    return render_template()
 
 
 if __name__ == "__main__":
